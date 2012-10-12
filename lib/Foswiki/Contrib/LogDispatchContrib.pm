736bf635ba456ab2a2b54a34c496faa591350ded
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-10-12 23:33:38 +0000

    Revert "Item11267: Convert to modern perl version string."
    This reverts commit e8ea2e26050d87294dc6218c68eff9fdb29c09c7.
    
    git-svn-id: http://svn.foswiki.org/trunk/LogDispatchContrib@15575 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/LogDispatchContrib.pm
+++ lib/Foswiki/Contrib/LogDispatchContrib.pm
@@ -15,7 +15,22 @@ package Foswiki::Contrib::LogDispatchContrib;
 use strict;
 use warnings;
 
-use version; our $VERSION = version->declare("v1.0.1");
+# $VERSION is referred to by Foswiki, and is the only global variable that
+# *must* exist in this package. This should always be in the format
+# $Rev$ so that Foswiki can determine the checked-in status of the
+# extension.
+our $VERSION = '$Rev$';
+
+# $RELEASE is used in the "Find More Extensions" automation in configure.
+# It is a manually maintained string used to identify functionality steps.
+# You can use any of the following formats:
+# tuple   - a sequence of integers separated by . e.g. 1.2.3. The numbers
+#           usually refer to major.minor.patch release or similar. You can
+#           use as many numbers as you like e.g. '1' or '1.2.3.4.5'.
+# isodate - a date in ISO8601 format e.g. 2009-08-07
+# date    - a date in 1 Jun 2009 format. Three letter English month names only.
+# Note: it's important that this string is exactly the same in the extension
+# topic - if you use %$RELEASE% with BuildContrib this is done automatically.
 our $RELEASE = '1.0.1';
 
 our $SHORTDESCRIPTION = 'Advanced logging using Log::Dispatch';
